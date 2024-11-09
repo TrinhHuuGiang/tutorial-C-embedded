7b57ff064205693eea4e651743d884822545fc96
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-11-09 23:30:26 +0700

    update Licenses

--- src/_09_KE16_Interrupt/led_green_status.h
+++ src/_09_KE16_Interrupt/led_green_status.h
@@ -1,7 +1,16 @@
 /*
+    Copyright (C) <2024> <Giang Trinh>
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+
+/*
  * do not modify system tick when using this library
  * using function init_Systick before using library
- * */
+ */
 /*******************************************************************************
 * Definitions
 ******************************************************************************/
@@ -48,3 +57,13 @@ void blink_Led_green();
 void raising_Led_green();
 
 #endif /* LED_GREEN_STATUS_H_ */
+
+/*
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
--- src/_11_KE16_Clock/MKE16Z4_LPIT/led_gb.c
+++ src/_11_KE16_Clock/MKE16Z4_LPIT/led_gb.c
@@ -1,3 +1,12 @@
+/*
+    Copyright (C) <2024> <Giang Trinh>
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+
 /*******************************************************************************
 * Definitions
 ******************************************************************************/
@@ -6,15 +15,6 @@
 #include <stdint.h>
 #define LED_GREEN     (4U)
 #define LED_BLUE      (1U)
-
-/*******************************************************************************
-* Variables
-******************************************************************************/
-
-/*******************************************************************************
-* Prototypes
-******************************************************************************/
-
 
 /*******************************************************************************
 * Code
@@ -49,3 +49,13 @@ void init_led_GREEN()
 	/*(Beginning) set output logic level 1 - off led (FGPIO4_PSOR)*/
 	FGPIOB->PSOR |= (1<<LED_GREEN);
 }
+
+/*
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
